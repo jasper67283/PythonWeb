3ac3a8974ee7f309906c46497385c5a0fd4ac14c
jasper67283 <jasperkuo07170717@gmail.com>
2025-02-07 15:21:32 +0800

    up

--- pages/class5-2.py
+++ pages/class5-2.py
@@ -23,7 +23,20 @@ if message:
     st.session_state.history.append({"role": "user", "content": message})
     completion = client.chat.completions.create(
         model="gpt-4o",
-        messages=[{"role": "developer", "content": "你是小明，只能用中文回應後續"}]
+        messages=[
+            {
+                "role": "developer",
+                "content": """
+                只能用繁體中文(#zh-TW)回應後續對話
+                請跟使用者玩一場猜謎遊戲，你需要先描述題目，接下來依照使用者回應的猜測給予提示
+                提示只能說[是]與[否]，不可以提供太多資訊
+                以下為猜謎遊戲的題目(一開始要提供給玩家的資訊):
+                題目你自己出
+                以下為答案(不能提供給玩家的資訊直到玩家差不多猜到的時候再公布答案):
+                答案你自己出
+               """,
+            }
+        ]
         + st.session_state.history,
     )
     st.session_state.history.append(
